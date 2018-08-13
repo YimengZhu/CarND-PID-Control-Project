21c236436bb19269b93a57bf7bcf5425634d8d53
Yimeng Zhu <yimengzhu@Yimengs-MacBook-Pro.local>
2018-08-13 08:57:59 +0200

    fixed bug

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.2, 0.01, 30); 
+  pid.Init(0.1, 0.001, 3.2); 
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -60,7 +60,7 @@ int main()
           */
 
           pid.UpdateError(cte);
-          steer_value -= pid.TotalError();
+          steer_value = -pid.TotalError();
           steer_value = fmin(fmax(steer_value, -1.0), 1.0);
 
 
